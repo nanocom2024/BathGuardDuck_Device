bb6494f8212170814953965205af2135eb189ce4
masaru-gif <k22089kk@aitech.ac.jp>
2024-08-27 18:36:40 +0900

    Bluetoothシリアル通信できるようにしました。
    Bluetoothシリアル通信できるようにしました。

--- src/selectMainFile.h
+++ src/selectMainFile.h
@@ -2,6 +2,10 @@
 //陸上と水上を区別するための実験用
 // #define SELECT_BLE
 
+//加速度センサの値をBluetooth Classicで送信するプログラム
+//陸上と水上を区別するための実験用
+#define SELECT_Bluetooth
+
 //加速度センサの値をWiFiでLINENotifyに送信するプログラム
 //本番はこっち
-#define SELECT_WIFI
+// #define SELECT_WIFI
