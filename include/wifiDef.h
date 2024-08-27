bc56cb5c66bd65b81a24f4282cc6f68a78c2c7d8
yuudati4 <104051133+yuudati4@users.noreply.github.com>
2024-08-27 14:52:25 +0900

    hiroshi

--- include/wifiDef.h
+++ include/wifiDef.h
@@ -6,17 +6,17 @@ const char* password = "";
 // const char* lineToken = ""; // ここにLine Notifyのトークンを入れる
 const char* notifyMes = "めっちゃ揺れてる！！！！！！！！！！！！";
 
-//プロトタイプ宣言
-// void sendLineNotify(String message);
+// プロトタイプ宣言
+//  void sendLineNotify(String message);
 void sendNotification();
 
 void wifiSetUp(const char* ssid, const char* password) {
-    // WiFiの設定
-    WiFi.begin(ssid, password);
-    Serial.print("Connecting to WiFi...");
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.print(".");
-    }
-    Serial.println("Connected to WiFi");
+  // WiFiの設定
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi");
 }
