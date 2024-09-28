5b014ab9982c4fec9187b3f939479865832f1e11
yuudati4 <104051133+yuudati4@users.noreply.github.com>
2024-09-28 09:18:02 +0900

    デバック用に10秒で水陸検知8秒で溺れ検知、BLUETOOTH状態

--- src/bluetooth/mainBluetooth.cpp
+++ src/bluetooth/mainBluetooth.cpp
@@ -17,11 +17,12 @@ void setup() {
 }
 
 // 変数をグローバルに宣言
-unsigned long startTimeAbove = 0; // 閾値超えた時間の計測
 unsigned long startTimeBelow = 0; // 閾値を下回った時間の計測
-bool isAbove = false; // zが閾値を超えているかどうか
+unsigned long startTimeAbove = 0; // 閾値を上回った時間の計測
+unsigned long startTimeWave = 0;
 bool isBelow = false; // zが閾値を下回っているかどうか
-
+bool isAbove = false; // zが閾値を上回っているかどうか
+bool isWave = false;
 int angle = 14000; // 水上に浮いているかどうかの閾値
 
 void loop() {
@@ -43,54 +44,45 @@ void loop() {
   double stdZ = getAccelStdZ(avgAccel);
 
   //Bluetoothで送信
-  SerialBT.print(nowAccel.x);
-  SerialBT.print(',');
-  SerialBT.print(nowAccel.y);
-  SerialBT.print(',');
   SerialBT.print(nowAccel.z);
   SerialBT.print(',');
-  SerialBT.print(std);
-  SerialBT.print(',');
-  SerialBT.print(stdX);
-  SerialBT.print(',');
-  SerialBT.print(stdY);
-  SerialBT.print(',');
-  SerialBT.println(stdZ);
-  
-  Serial.print(">avg:");
-  Serial.println(std);
+  SerialBT.println(std);
 
-// // 水上に浮いているかどうかの判定
-//     if (nowAccel.z >= angle) {
-//         if (!isAbove) {
-//             // 初めて閾値を超えたら時間を記録
-//             startTimeAbove = millis();
-//             isAbove = true;
-//         } else if (millis() - startTimeAbove >= 3000) {
-//             // 閾値を超え続けて2分が経過したら「水上に浮いている」と判定
-//             SerialBT.println("水上に浮いている");
-//             // 必要な処理をここに記述
-//         }
-//     } else {
-//         // 閾値を下回ったらリセット
-//         isAbove = false;
-//     }
+    // 陸上に設置されているかどうかの判定
+    if (nowAccel.z < angle) {
+        if (!isBelow) {
+            // 初めて閾値を下回ったら時間を記録
+            startTimeBelow = millis();
+            isBelow = true;
+        } else if (millis() - startTimeBelow >= 10000) {
+            // 閾値を下回り続けて2分が経過したら「陸上に設置されている」と判定
+            SerialBT.println("陸上に設置されている");
+            // 必要な処理をここに記述
+        }
+    } else {
+        // 閾値を超えたらリセット
+        isBelow = false;
+    }
 
-//     // 陸上に設置されているかどうかの判定
-//     if (nowAccel.z < angle) {
-//         if (!isBelow) {
-//             // 初めて閾値を下回ったら時間を記録
-//             startTimeBelow = millis();
-//             isBelow = true;
-//         } else if (millis() - startTimeBelow >= 3000) {
-//             // 閾値を下回り続けて2分が経過したら「陸上に設置されている」と判定
-//             SerialBT.println("陸上に設置されている");
-//             // 必要な処理をここに記述
-//         }
-//     } else {
-//         // 閾値を超えたらリセット
-//         isBelow = false;
-//     }
+    if (nowAccel.z >= angle) {
+        if (!isAbove) {
+        startTimeAbove = millis();
+        isAbove = true;
+        } else if (millis() - startTimeAbove >= 10000) { // 2分に変更
+        // 閾値を超え続けて2分が経過したら「水上に浮いている」と判定
+            SerialBT.println("水上に浮いている");
+            if (std > 1000) {
+                startTimeWave = millis();
+                isWave = true;
+            } else if (millis() - startTimeWave >= 9000 && isWave) {
+                SerialBT.println("溺れ検知");
+                isWave = false; // リセット
+                delay(5000);
+            }
+        }
+    } else {
+    isAbove = false;
+    }
 
   //遅延
   delay(20);
